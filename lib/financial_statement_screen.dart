81f53172e8aa1946cc453639af0e83de48de69a3
jalaprthi1 <alaparthijoshika@gmail.com>
2025-03-27 15:36:47 -0400

    Added financial screen

---
+++ lib/financial_statement_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class FinancialStatementScreen extends StatefulWidget {
+  @override
+  _FinancialStatementScreenState createState() =>
+      _FinancialStatementScreenState();
+}
+
+class _FinancialStatementScreenState extends State<FinancialStatementScreen> {
+  DateTime _selectedDay = DateTime.now();
+  Map<DateTime, List<String>> transactions = {
+    DateTime(2025, 3, 20): ["Salary: +\$2000", "Groceries: -\$150"],
+    DateTime(2025, 3, 21): ["Electricity Bill: -\$80"],
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Financial Statement")),
+      body: Column(
+        children: [
+          TableCalendar(
+            focusedDay: DateTime.now(),
+            firstDay: DateTime(2020),
+            lastDay: DateTime(2030),
+            selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+              });
+            },
+          ),
+          Expanded(
+            child: ListView(
+              children: (transactions[_selectedDay] ?? [])
+                  .map((transaction) => ListTile(title: Text(transaction)))
+                  .toList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
