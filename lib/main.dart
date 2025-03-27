6017c8dbadcfa6a56a12320009fa8c9d2be9b2d1
jalaprthi1 <alaparthijoshika@gmail.com>
2025-03-27 14:34:52 -0400

    Changed main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
 import 'login_screen.dart';
+import 'account_setup_screen.dart';
+import 'budget_savings_screen.dart'; // Correct import for BudgetSavingsScreen
+import 'financial_statement_screen.dart';
+import 'profile_settings_screen.dart'; // Correct import for ProfileSettingsScreen
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Financial App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        useMaterial3: true,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: LoginScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginScreen(),
+        '/account_setup': (context) => AccountSetupScreen(),
+        '/budget': (context) =>
+            BudgetScreen(), // Correct route for BudgetSavingsScreen
+        '/financial_statement': (context) => FinancialStatementScreen(),
+        '/profile_setting': (context) =>
+            ProfileScreen(), // Correct route for ProfileSettingsScreen
+      },
     );
   }
 }
